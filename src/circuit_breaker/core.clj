41fcef8fd7cffa657b2940a6955074cb996d00cd
josephwilk <joe@josephwilk.net>
2013-02-05 20:17:18 +0100

    Refactor: Keep public methods at the bottom

--- src/circuit_breaker/core.clj
+++ src/circuit_breaker/core.clj
@@ -28,28 +28,11 @@
 (defn- record-opening! [circuit-name]
   (swap! _circuit-breakers-open assoc circuit-name (atom(time/now))))
 
-(defn record-failure [circuit-name]
-  (inc-counter circuit-name)
-  (when (> (failure-count circuit-name) (failure-threshold circuit-name))
-    (record-opening! circuit-name)))
-
-(defn record-success [circuit-name]
-  (swap! _circuit-breakers-open assoc circuit-name (atom nil))
-  (swap! _circuit-breakers-counters assoc circuit-name (atom 0)))
-
 (defn- breaker-open? [circuit-name]
   (not (not (time-since-broken circuit-name))))
 
 (defn- timeout-exceeded? [circuit-name]
   (> (time/in-secs (time/interval (time-since-broken circuit-name) (time/now))) (timeout-in-seconds circuit-name)))
-
-(defn tripped? [circuit-name]
-  (and (breaker-open? circuit-name) (not (timeout-exceeded? circuit-name))))
-
-(defn defncircuitbreaker [circuit-name settings]
-  (swap! _circuit-breakers-counters merge {circuit-name (atom 0)})
-  (swap! _circuit-breakers-config   merge {circuit-name (atom settings)})
-  (swap! _circuit-breakers-open     merge {circuit-name (atom nil)}))
 
 (defn- closed-circuit-path [circuit-name method-that-might-error]
   (try
@@ -59,6 +42,23 @@
     (catch Exception e
       (logger/error e)
       (record-failure circuit-name))))
+
+(defn record-failure [circuit-name]
+  (inc-counter circuit-name)
+  (when (> (failure-count circuit-name) (failure-threshold circuit-name))
+    (record-opening! circuit-name)))
+
+(defn record-success [circuit-name]
+  (swap! _circuit-breakers-open assoc circuit-name (atom nil))
+  (swap! _circuit-breakers-counters assoc circuit-name (atom 0)))
+
+(defn tripped? [circuit-name]
+  (and (breaker-open? circuit-name) (not (timeout-exceeded? circuit-name))))
+
+(defn defncircuitbreaker [circuit-name settings]
+  (swap! _circuit-breakers-counters merge {circuit-name (atom 0)})
+  (swap! _circuit-breakers-config   merge {circuit-name (atom settings)})
+  (swap! _circuit-breakers-open     merge {circuit-name (atom nil)}))
 
 (defn wrap-with-circuit-breaker [circuit-name method-that-might-error &[default-method]]
   (if (tripped? circuit-name)
